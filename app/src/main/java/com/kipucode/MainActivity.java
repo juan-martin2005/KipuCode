e4b5c90c9e403e7ec40357ecc1caa01236d00fc0
Marcelino <mmamanipalma369@gmail.com>
2026-05-05 16:43:43 -0500

    feat: Implementar pantalla de bienvenida con soporte para modo claro y oscuro.
    Se agrego una vista de Bienvenida en el MainActivity con redireccionamiento a inicio de sesión y registro.
    Se implemento funcionalidad de cambio de tema (Modo Claro/Oscuro).

--- app/src/main/java/com/kipucode/MainActivity.java
+++ app/src/main/java/com/kipucode/MainActivity.java
@@ -1,20 +1,35 @@
 package com.kipucode;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.kipucode.service.Utils;
 import com.kipucode.view.Login;
 import com.kipucode.view.Register;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvHero, tvSubHead;
+    Button btnLogin, btnCreateAccount;
+    ImageView ivMode;
+    TypedValue typedValue = new TypedValue();
+    int currentMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +42,64 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Intent goLogin = new Intent(MainActivity.this, Register.class);
-        startActivity(goLogin);
+        ivMode = findViewById(R.id.iv_mode);
+
+        currentMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+
+        if (currentMode == Configuration.UI_MODE_NIGHT_YES){
+            ivMode.setImageResource(R.drawable.light_mode);
+        }else {
+            ivMode.setImageResource(R.drawable.dark_mode);
+        }
+        changeTheme();
+
+        tvHero = findViewById(R.id.tv_heroHead);
+        tvSubHead = findViewById(R.id.subHeadline);
+
+        btnCreateAccount = findViewById(R.id.btn_createAccount);
+        btnLogin = findViewById(R.id.btn_logIn);
+
+
+        getTheme().resolveAttribute(android.R.attr.textColorLink, typedValue, true);
+
+        loginIntent();
+        createAccountIntent();
+        colorHero();
+    }
+
+    public void colorHero(){
+        int colorEnlace = typedValue.data;
+        String _hero = tvHero.getText().toString();
+        SpannableString spannableString = Utils.getColoredText(_hero, "code", colorEnlace);
+
+        tvHero.setText(spannableString);
+    }
+
+    public void changeTheme(){
+        ivMode.setOnClickListener(v -> {
+            if (currentMode == Configuration.UI_MODE_NIGHT_YES){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                ivMode.setImageResource(R.drawable.light_mode);
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                ivMode.setImageResource(R.drawable.dark_mode);
+            }
+            recreate();
+        });
+    }
+
+    public void loginIntent(){
+        btnLogin.setOnClickListener(v -> {
+            Intent login = new Intent(this, Login.class);
+            this.startActivity(login);
+        });
+    }
+
+    public void createAccountIntent(){
+        btnCreateAccount.setOnClickListener(v -> {
+            Intent createAccount = new Intent(this, Register.class);
+            this.startActivity(createAccount);
+        });
     }
 
 }
