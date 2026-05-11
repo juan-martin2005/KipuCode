5dd878f8620777cde9062fdf42a0142493060db6
Marcelino <mmamanipalma369@gmail.com>
2026-05-11 12:30:38 -0500

    feat: Actualizar paleta de colores y restaurar slider.
    Se configuraron los nuevos colores primarios y tipográficos en los themes claro y oscuro.
    Se restauró la lógica e interfaz del slider en Onboarding, antes MainActivity.
    Se eliminó el recurso gráfico kipu.png que ya no era utilizado.

--- app/src/main/java/com/kipucode/view/Onboarding.java
+++ app/src/main/java/com/kipucode/view/Onboarding.java
@@ -15,14 +15,23 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.kipucode.R;
+import com.kipucode.utils.SliderTextAdapter;
 import com.kipucode.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Onboarding extends AppCompatActivity {
 
-    TextView tvHero, tvSubHead;
+    TextView tvHero;
+    ViewPager2 viewPager;
+    TabLayout tabLayout;
     Button btnLogin, btnCreateAccount;
     ImageView ivMode;
     TypedValue typedValue = new TypedValue();
@@ -38,6 +47,16 @@ public class Onboarding extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        viewPager = findViewById(R.id.viewPager_Slider);
+        tabLayout = findViewById(R.id.tabLayout_Dots);
+
+        SliderTextAdapter adapter = getSliderTextAdapter();
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {
+                }
+        ).attach();
 
         ivMode = findViewById(R.id.iv_mode);
 
@@ -51,7 +70,6 @@ public class Onboarding extends AppCompatActivity {
         changeTheme();
 
         tvHero = findViewById(R.id.tv_heroHead);
-        tvSubHead = findViewById(R.id.subHeadline);
 
         btnCreateAccount = findViewById(R.id.btn_createAccount);
         btnLogin = findViewById(R.id.btn_logIn);
@@ -61,13 +79,33 @@ public class Onboarding extends AppCompatActivity {
 
         loginIntent();
         createAccountIntent();
-        colorHero();
     }
 
-    public void colorHero(){
+    private SliderTextAdapter getSliderTextAdapter() {
+        List<String> listaSubTextos = new ArrayList<>();
+
+        listaSubTextos.add("Build real programming skills through guided lessons and hands on experience");
+        listaSubTextos.add("Master fundamental logic through structured and easy to follow learning paths");
+        listaSubTextos.add("Transition from theory to execution with interactive coding challenges");
+        listaSubTextos.add("Transform basic knowledge into advanced & professional engineering skills.");
+
+        final String[] palabrasClave = {"code", "clarity", "practice", "purpose"};
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                colorHero(palabrasClave[position]);
+            }
+        });
+
+        return new SliderTextAdapter(listaSubTextos);
+    }
+
+    public void colorHero(String text){
         int colorEnlace = typedValue.data;
         String _hero = tvHero.getText().toString();
-        SpannableString spannableString = Utils.getColoredText(_hero, "code", colorEnlace);
+
+        SpannableString spannableString = Utils.getColoredText(_hero, text, colorEnlace);
 
         tvHero.setText(spannableString);
     }
