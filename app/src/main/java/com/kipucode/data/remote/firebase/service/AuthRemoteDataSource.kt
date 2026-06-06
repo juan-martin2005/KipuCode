26fd72dbc6a192696aa1b4a0b1e847c3e8eb015a
Marcelino <mmamanipalma369@gmail.com>
2026-06-06 06:15:43 -0500

    fix: Resolver bug de borrado en cascada (CASCADE) y forzar jerarquía de sincronización en Auth
    - Eliminado ON DELETE CASCADE de las foreign keys de Lesson y UserProgress para evitar que la
    estrategia REPLACE de Room borre datos locales al actualizar.
    - Refactorizado AuthRepositoryImpl para la inserción de datos al iniciar sesión:
    Courses -> Lessons -> User Profile & Progress.

--- app/src/main/java/com/kipucode/data/remote/firebase/service/AuthRemoteDataSource.kt
+++ app/src/main/java/com/kipucode/data/remote/firebase/service/AuthRemoteDataSource.kt
@@ -5,26 +5,51 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
+// ==========================================================================================
+//  ORIGEN DE AUTENTIFICACIÓN - FIREBASEAUTH
+// ==========================================================================================
 class AuthRemoteDataSource @Inject constructor(
+    // ======================================================================================
+    //  Instancia de FirebaseAuth   ->  Gestionar sesión.
+    // ======================================================================================
     private val firebaseAuth: FirebaseAuth
 ) {
-    suspend fun signInWithEmail(
-        email: String, password: String
-    ): AuthResult = firebaseAuth.signInWithEmailAndPassword(email, password).await()
 
-    suspend fun registerUserWithEmail(
-        email: String, password: String
-    ): AuthResult {
+    // ======================================================================================
+    //  Registro de usuario -> FirebaseAuth solo trabaja con (Correo y Contraseña)
+    // ======================================================================================
+    suspend fun registerUserWithEmail(email: String, password: String): AuthResult {
+        // ! IMPORTANTE
+        // Al hacer el Registro, firebase automáticamente guarda el UID del usuario
+        // Por eso se puede user (firebaseAuth.currentUser) para el envío del email de verificación
+        // En la Implementación de esta función IMPORTANTE cerrar la sesión del usuario.
         val result = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
         firebaseAuth.currentUser?.sendEmailVerification()?.await()
         return result
     }
 
+    // ======================================================================================
+    //  Inicio de sesión -> FirebaseAuth solo trabaja con (Correo y Contraseña)
+    // ======================================================================================
+    suspend fun signInWithEmail(email: String, password: String): AuthResult =
+        firebaseAuth
+        .signInWithEmailAndPassword(email, password)
+        .await()
+
+    // ======================================================================================
+    //  Cambiar contraseña -> FirebaseAuth incluye una función sendPasswordResetEmail
+    // ======================================================================================
     suspend fun sendPasswordReset(email: String) {
         firebaseAuth.sendPasswordResetEmail(email).await()
     }
 
+    // ======================================================================================
+    //  Utilidad Extra -> Verificar si hay una sesión activa en el dispositivo
+    // ======================================================================================
     fun isUserLoggedIn(): Boolean = firebaseAuth.currentUser != null
 
+    // ======================================================================================
+    //  Funcionalidad Extra -> Cerrar la sesión del dispositivo
+    // ======================================================================================
     fun logoutUser() = firebaseAuth.signOut()
 }
--- app/src/main/java/com/kipucode/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/kipucode/domain/repository/AuthRepository.kt
@@ -1,11 +1,11 @@
 package com.kipucode.domain.repository
 
 import com.kipucode.domain.model.Response
-import com.kipucode.domain.model.User
+import com.kipucode.domain.model.UserDomain
 
 interface AuthRepository {
-    suspend fun login(email: String, password: String): Response<User>
-    suspend fun register(user: User, password: String): Response<User>
+    suspend fun login(email: String, password: String): Response<UserDomain>
+    suspend fun register(userDomain: UserDomain, password: String): Response<UserDomain>
     suspend fun resetPassword(email: String): Response<Unit>
     suspend fun logout()
     fun isUserLoggedIn(): Boolean
