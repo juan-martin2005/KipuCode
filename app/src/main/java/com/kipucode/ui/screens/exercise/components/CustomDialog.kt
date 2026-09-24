781569464d4421a1f0ecd56362bbf81d4413754e
Juan <martin.gonzaless2005@gmail.com>
2026-09-23 22:44:58 -0500

    refactor: reorganizar pantallas y componentes del paquete exercise
    - Mover la pantalla ExerciseScreen del paquete lesson al paquete exercise.
    
    - Trasladar los componentes Choices, CustomDialog y OptionsCard a ui.screens.exercise.components.
    
    - Actualizar rutas de importacion para reflejar la nueva organizacion modular.

--- app/src/main/java/com/kipucode/ui/screens/exercise/components/CustomDialog.kt
+++ app/src/main/java/com/kipucode/ui/screens/exercise/components/CustomDialog.kt
@@ -1,4 +1,4 @@
-package com.kipucode.ui.screens.lesson.components
+package com.kipucode.ui.screens.exercise.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
--- app/src/main/java/com/kipucode/ui/screens/exercise/components/OptionsCard.kt
+++ app/src/main/java/com/kipucode/ui/screens/exercise/components/OptionsCard.kt
@@ -1,4 +1,4 @@
-package com.kipucode.ui.screens.lesson.components
+package com.kipucode.ui.screens.exercise.components
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
